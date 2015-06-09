69398bff3fa7c6a573d1be51b7fe910bb79f9175
unknown <wangfangfang_yy@CY11144.cyou-inc.com>
2015-06-09 18:23:06 +0800

    1

--- src/main/webapp/resources/js/rights_manage.js
+++ src/main/webapp/resources/js/rights_manage.js
@@ -3,22 +3,7 @@ $(function(){
 	$("#btn-save").on("click",function(){
 		var username=$("#recipient-name").val();
 		if(username){
-			$.ajax({
-				url :"hasuser.json",
-				type : 'POST',
-				dataType : 'json',
-				async : false,
-				data : {
-					username:username
-				},
-				success : function(data) {
-					if(data.hasuser=="1"){
-						$("#form-save").submit();
-					}else{
-						alert("请添加畅游内部邮箱前缀!");
-					}
-				}
-			});
+			$("#form-save").submit();
 		}
 	})
 //del赋值事件
