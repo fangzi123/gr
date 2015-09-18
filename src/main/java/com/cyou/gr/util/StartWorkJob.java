750d84dd4697c757d55061cd943334b7f45ad3c5
wff <wangfangfang_yy@cyou-inc.com>
2015-09-18 13:55:13 +0800

    quartz

---
+++ src/main/java/com/cyou/gr/util/StartWorkJob.java
@@ -0,0 +1,10 @@
+package com.cyou.gr.util;
+
+import java.util.Date;
+
+public class StartWorkJob {
+	public void startWork() {
+		System.out.println(new Date());
+		System.out.println("上班了！~");
+	}
+}
